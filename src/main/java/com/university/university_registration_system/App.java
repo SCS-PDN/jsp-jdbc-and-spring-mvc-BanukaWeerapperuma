1a67b48f6a86a32cf2a4d6148c59ff06e2a1ae2f
BanukaWeerapperuma <banukaweerapperuma@gmail.com>
2025-05-09 23:23:05 +0530

    Initial commit on branch s20546

---
+++ src/main/java/com/university/university_registration_system/App.java
@@ -0,0 +1,13 @@
+package com.university.university_registration_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
